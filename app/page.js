25aacb368a4ac244075f89c41b61c1eba07a0a37
Mfahad159 <mianfahad664@gmail.com>
2025-08-18 21:23:44 +0500

    Added: Footer

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ import About from "./components/About";
 import Services from "./components/Services";
 import Work from "./components/Work";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 
 export default function Home() {
@@ -17,6 +18,7 @@ export default function Home() {
      <Services/>
      <Work/>
      <Contact/>
+     <Footer/>
      </>
   );
 }
