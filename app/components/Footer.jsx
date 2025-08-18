d11469701ec4d36b6e53fa8a844bdb0d43984e61
Mfahad159 <mianfahad664@gmail.com>
2025-08-19 00:22:57 +0500

    Adjust footer!

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className='mt-20'>
         <div className='text-center'>
             <Image src={assets.logo} alt='' className='w-36 mx-auto mb-2 '/>
-            <div className='w-max flex items-center gap-2 max-auto'>
+            <div className='w-max flex items-center gap-2 mx-auto'>
                 <Image src={assets.mail_icon} alt='' className='w-6'></Image>
                 mianfahad664@gmail.com
             </div>
