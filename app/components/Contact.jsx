674a3b1a5b7ead1be78b3dcad26e40ec10a5d376
Mfahad159 <mianfahad664@gmail.com>
2025-08-20 00:31:39 +0500

    Contact Fixed!

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { useState } from 'react'
 import Image from 'next/image'
 
-const Contact = () => {
+const Contact = ({isDarkMode}) => {
 
     const [result, setResult] = useState("");
 
@@ -31,7 +31,11 @@ const Contact = () => {
     };
 
 	return (
-		<div id='contact' className='w-full px-[12%] py-10 scroll-mt-20 bg-no-repeat bg-center' style={{ backgroundImage: 'url(/footer-bg-color.png)', backgroundSize: '90% auto' }}>
+		<div id='contact' className={`w-full px-[12%] py-10 scroll-mt-20 bg-no-repeat bg-center`} 
+     style={{ 
+       backgroundImage: isDarkMode ? 'none' : 'url(/footer-bg-color.png)', 
+       backgroundSize: '90% auto' 
+     }}>
 			<h4 className='text-center mb-2 text-lg font-ovo'>Connect with me</h4>
 			<h2 className='text-center text-5xl font-ovo'>Get in Touch</h2>
 			<p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo'>
@@ -40,15 +44,15 @@ const Contact = () => {
 			</p>
             <form onSubmit={onSubmit} className='mx-2-2xl mx-auto'>
                 <div className='grid grid-cols-auto gap-6 mt-10 mb-8'>
-                    <input name='name' className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 
-                     rounded-md bg-white' type="text" placeholder='Enter your name' required/>
-                    <input name='email' className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 
-                     rounded-md bg-white' type="email" placeholder='Enter your email' required/>
+                    <input name='name' className={`flex-1 p-3 outline-none border-[0.5px] border-gray-400 
+                     rounded-md bg-white ${isDarkMode?"border-white/90":""}`} type="text" placeholder='Enter your name' required/>
+                    <input name='email' className={`flex-1 p-3 outline-none border-[0.5px] border-gray-400 
+                     rounded-md bg-white ${isDarkMode?"border-white/90":""}`} type="email" placeholder='Enter your email' required/>
                 </div>
-                <textarea name='message' className='w-full p-4 outline-none border-[0.5px] border-gray-400 
-                     rounded-md bg-white mb-6' rows='6' placeholder='Enter your message' required></textarea>
-                <button  className='py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80
-                                    text-white rounded-full mx-auto hover:bg-black duration-500 cursor-pointer'
+                <textarea name='message' className={`w-full p-4 outline-none border-[0.5px] border-gray-400 
+                     rounded-md bg-white mb-6 ${isDarkMode?"border-white/90":""}`} rows='6' placeholder='Enter your message' required></textarea>
+                <button  className={`py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80
+                                    text-white rounded-full mx-auto hover:bg-black duration-500 cursor-pointer ${isDarkMode?"bg-transparent border-[0.5px] border-white/90 hover:bg-darkhover ":""}`}
                 type='submit'>Submit now <Image src={assets.right_arrow_white} alt='' className='w-4'></Image></button>
 
                 <p className='mt-4 text-center'>{result}</p>
