03d2e7004c4db2161b46fdc0ee241a99312dab5c
Mfahad159 <mianfahad664@gmail.com>
2026-08-13 01:20:29 +0500

    Fixed theme colour

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ import SectionDivider from './components/ui/SectionDivider';
 import { useEffect, useState } from 'react';
 
 export default function Home() {
-  const [isDarkMode, setIsDarkMode] = useState(true);
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   useEffect(() => {
     const stored = localStorage.theme;
